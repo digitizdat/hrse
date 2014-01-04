0de37280da8e0cffb859326a7cc0121a406193b9
Martin McGreal <martin@mcgreal.org>
2014-01-04 05:10:42 +0000

    This got missed in the last commit.
    Update py/controller with the new submit() method, which takes the
    input sequence and returns a response page.  The next step is to develop
    this method (with additional functions) to deliver a useful set of metrics
    about the input sequence along with the response page.

--- py/controller.py
+++ py/controller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import operator, os, pickle, sys
+import json
 import cherrypy
 from genshi.template import TemplateLoader
 
@@ -13,17 +14,53 @@ loader = TemplateLoader(
 )
 
 
+def getval(spec, jdict=None):
+    """Retrieve the value of the specified attribute.
+
+    The attribute to retrieve is specified via the spec parameter, where:
+       spec: 'parent1:parent2:...:parentN:valname'
+
+    The jdict parameter is the dictionary of dictionaries to extract the
+    value from.  Typically this would be the decoded JSON document that was
+    passed into the web service.
+
+    If the value is not found, this function will raise a KeyError.
+
+    """
+    if jdict is None:
+        raise RuntimeError, "getval(): You passed me an empty jdict!"
+
+    if ':' in spec:
+        valpath = spec.split(':')
+        return getval(':'.join(valpath[1:]),
+                           jdict[valpath[0]])
+
+    return jdict[spec]
+
+
 class Root(object):
+    """This is the default root object needed for a CherryPy web instance."""
     def __init__(self, data):
         self.data = data
 
     @cherrypy.expose
     def index(self):
         tmpl = loader.load('index.html')
-        return tmpl.generate(title='Human Random Sequence Experiment').render('html', doctype='html', strip_whitespace=False)
+
+        return tmpl.generate().render('html', doctype='html', strip_whitespace=False)
+
+    @cherrypy.expose
+    def submit(self, **kwargs):
+        """Analyze the submitted binary sequence and generate a response page."""
+
+        data = json.loads(cherrypy.request.body.read())
+        print "submit called with: "+str(data)
+        sequence = getval("sequence", data)
+        tmpl = loader.load('submission.html')
+        return tmpl.generate(submission=sequence).render('html', doctype='html', strip_whitespace=False)
 
 
-def main(filename):
+def main():
     data = {} # We'll replace this later
 
     # Some global configuration; note that this could be moved into a
@@ -57,5 +94,5 @@ def main(filename):
     })
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
 
